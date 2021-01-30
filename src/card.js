fb86024bd9539e8f6be18d8a97685bf3548568ce
Filipe André <filipeandres@hotmail.com>
2021-01-30 18:34:26 -0300

    Improving styles

--- src/card.js
+++ src/card.js
@@ -4,7 +4,8 @@ import './card.scss'
 const Card = (props) => {
     return (
         <div className="card">
-            <div style={{ backgroundImage: `url(${props.url})` }} className="card--image"></div>
+            <div className="card--image" style={{ backgroundImage: `url(${props.url})` }}></div>
+            <div className="card--overlay"></div>
             <div className="card--content">
                 <span className="campaign">{props.campaign}</span>
                 {/* <span className="system">{props.sistema}</span> */}

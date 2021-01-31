bcd34b9ccbd60970dbdf31c86514fd34e52a21e8
Filipe André <filipeandres@hotmail.com>
2021-01-31 13:49:06 -0300

    Improving Styles

--- src/search.js
+++ src/search.js
@@ -4,7 +4,27 @@ import './search.scss'
 const Search = () => {
     return (
         <div className="search-wrapper">
-            <div id="search" className="search">Encontre sua próxima mesa...</div>
+            <div id="search" className="search">
+                <div className="search-1">
+                    <span>Localização</span>
+                    <input type="text" placeholder="Onde você quer jogar?" />
+                </div>
+
+                <div className="search-2">
+                    <span>Check-in</span>
+                    <input type="text" placeholder="Insira as datas" />
+                </div>
+
+                <div className="search-3">
+                    <span>Checkout</span>
+                    <input type="text" placeholder="Insira as datas" />
+                </div>
+
+                <div className="search-4">
+                    <span>Players</span>
+                    <input type="text" placeholder="Onde você quer jogar?" />
+                </div>
+            </div>
         </div>
     );
 }
